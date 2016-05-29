758d723c7c722514b4af213a34f36d5a0047d733
Satyarth Sampath <satyarth@wooplr.com>
2016-05-30 00:42:06 +0530

    updated files

--- app/src/main/java/app/droidekas/fittest/activities/MainActivity.java
+++ app/src/main/java/app/droidekas/fittest/activities/MainActivity.java
@@ -7,7 +7,6 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,20 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.data.DataSet;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.request.DataReadRequest;
-import com.google.android.gms.fitness.result.DataReadResult;
 import com.google.android.gms.location.LocationServices;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 import app.droidekas.fittest.Global;
 import app.droidekas.fittest.R;
@@ -37,14 +24,10 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 import static app.droidekas.fittest.log.Logger.logger;
-import static app.droidekas.fittest.log.Logger.printData;
-import static java.text.DateFormat.getDateInstance;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final String AUTH_PENDING = "auth_state_pending";
-    private static boolean authInProgress = false;
     private static final String TAG = MainActivity.class.getSimpleName();
     private final int LOCATION_PERMISSION_REQUEST = 20;
     public static GoogleApiClient mClient = null;
@@ -69,9 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 requestLocationPermission();
             }
         }
-        next.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, RecordsActivity.class));
-        });
+        next.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RecordsActivity.class)));
     }
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -80,12 +61,9 @@ public class MainActivity extends AppCompatActivity {
             if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
                     || shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
                 Global.snackBar("App needs to access you location", tvTest);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST);
-                    }
-                }, Toast.LENGTH_LONG + 500);
+                new Handler().postDelayed(() ->
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST)
+                        , Toast.LENGTH_LONG + 500);
             } else {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST);
             }
@@ -94,36 +72,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Build a {@link GoogleApiClient} that will authenticate the user and allow the application
-     * to connect to Fitness APIs. The scopes included should match the scopes your app needs
-     * (see documentation for details). Authentication will occasionally fail intentionally,
-     * and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
-     * can address. Examples of this include the user never having signed in before, or
-     * having multiple accounts on the device and needing to specify which account to use, etc.
-     */
     private void buildGoogleClient() {
         // Create the Google API Client
         mClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.HISTORY_API)
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(
                         new GoogleApiClient.ConnectionCallbacks() {
                             @Override
                             public void onConnected(Bundle bundle) {
                                 logger(TAG, "Connected!!!");
-                                // Now you can make calls to the Fitness APIs.  What to do?
-                                // Look at some data!!
-
-//                                new InsertAndVerifyDataTask().execute();
-
                             }
 
                             @Override
                             public void onConnectionSuspended(int i) {
-                                // If your connection to the sensor gets lost at some point,
-                                // you'll be able to determine the reason and react to it here.
                                 if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
                                     logger(TAG, "Connection lost.  Cause: Network Lost.");
                                 } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
@@ -132,90 +93,16 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                 )
-                .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(ConnectionResult result) {
-                        logger(TAG, "Google Play services connection failed. Cause: " +
-                                result.toString());
+                .enableAutoManage(this, 0, result -> {
+                    logger(TAG, "Google Play services connection failed. Cause: " +
+                            result.toString());
 
-                        Global.snackBar("Exception while connecting to Google Play services: " +
-                                result.getErrorMessage(), tvTest);
-                    }
+                    Global.snackBar("Exception while connecting to Google Play services: " +
+                            result.getErrorMessage(), tvTest);
                 })
                 .build();
     }
 
-    /**
-     * Create a {@link DataSet} to insert data into the History API, and
-     * then create and execute a {@link DataReadRequest} to verify the insertion succeeded.
-     * By using an {@link AsyncTask}, we can schedule synchronous calls, so that we can query for
-     * data after confirming that our insert was successful. Using asynchronous calls and callbacks
-     * would not guarantee that the insertion had concluded before the read request was made.
-     * An example of an asynchronous call using a callback can be found in the example
-     * on deleting data below.
-     */
-    private class InsertAndVerifyDataTask extends AsyncTask<Void, Void, Void> {
-        protected Void doInBackground(Void... params) {
-            // Create a new dataset and insertion request.
-
-            // [START insert_dataset]
-            // Then, invoke the History API to insert the data and await the result, which is
-            // possible here because of the {@link AsyncTask}. Always include a timeout when calling
-            // await() to prevent hanging that can occur from the service being shutdown because
-            // of low memory or other conditions.
-
-            // Begin by creating the query.
-            DataReadRequest readRequest = queryFitnessData();
-
-            // [START read_dataset]
-            // Invoke the History API to fetch the data with the query and await the result of
-            // the read request.
-            DataReadResult dataReadResult =
-                    Fitness.HistoryApi.readData(mClient, readRequest).await(1, TimeUnit.MINUTES);
-            // [END read_dataset]
-
-            // For the sake of the sample, we'll print the data so we can see what we just added.
-            // In general, logging fitness information should be avoided for privacy reasons.
-            printData(dataReadResult, TAG);
-
-            return null;
-        }
-    }
-
-
-    /**
-     * Return a {@link DataReadRequest} for all step count changes in the past week.
-     */
-    public static DataReadRequest queryFitnessData() {
-        // [START build_read_data_request]
-        // Setting a start and end date using a range of 1 week before this moment.
-        Calendar cal = Calendar.getInstance();
-        Date now = new Date();
-        cal.setTime(now);
-        long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.WEEK_OF_YEAR, -1);
-        long startTime = cal.getTimeInMillis();
-
-        java.text.DateFormat dateFormat = getDateInstance();
-        logger(TAG, "Range Start: " + dateFormat.format(startTime));
-        logger(TAG, "Range End: " + dateFormat.format(endTime));
-
-        DataReadRequest readRequest = new DataReadRequest.Builder()
-                // The data request can specify multiple data types to return, effectively
-                // combining multiple data queries into one call.
-                // In this example, it's very unlikely that the request is for several hundred
-                // datapoints each consisting of a few steps and a timestamp.  The more likely
-                // scenario is wanting to see how many steps were walked per day, for 7 days.
-                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                // Analogous to a "Group By" in SQL, defines how data should be aggregated.
-                // bucketByTime allows for a time span, whereas bucketBySession would allow
-                // bucketing by "sessions", which would need to be defined in code.
-                .bucketByTime(1, TimeUnit.DAYS)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-                .build();
-        // [END build_read_data_request]
-        return readRequest;
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -240,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (resultCode == Activity.RESULT_OK) {
         }
     }
@@ -248,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
     public void checkAndStartService() {
         AppService service = new AppService();
         mServiceIntent = new Intent(getApplicationContext(), service.getClass());
-        if (!isMyServiceRunning(service.getClass())) {
+        if (!isServiceRunning(service.getClass())) {
             startService(mServiceIntent);
         }
     }
@@ -261,15 +147,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
+    private boolean isServiceRunning(Class<?> serviceClass) {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
             if (serviceClass.getName().equals(service.service.getClassName())) {
-                logger(TAG, "isMyServiceRunning? %s", true + "");
+
                 return true;
             }
         }
-        logger(TAG, "isMyServiceRunning? %s", false + "");
         return false;
     }
 
